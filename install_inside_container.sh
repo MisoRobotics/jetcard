e346b1b803fd1b1bac6a15ba87501b2d88adb3bd
Farrukh Ali <farrukh88@gmail.com>
2020-06-15 16:35:09 -0700

    Add required and remove extra packages

--- install_inside_container.sh
+++ install_inside_container.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-set -e
+#set -e
 
-password='jetson'
+#password='jetson'
 
 # Record the time this script starts
 date
 
 # Get the full dir name of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Keep updating the existing sudo time stamp
 sudo -v
 while true; do sudo -n true; sleep 120; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Enable i2c permissions
-echo "\e[100m Enable i2c permissions \e[0m"
-sudo usermod -aG i2c $USER
+## Enable i2c permissions
+#echo "\e[100m Enable i2c permissions \e[0m"
+#sudo usermod -aG i2c $USER
 
 # Install pip and some python dependencies
 echo "\e[104m Install pip and some python dependencies \e[0m"
@@ -33,8 +33,9 @@ sudo -H pip install jetson-stats
 # Install misc packages
 sudo apt install htop
 sudo apt install -y nano
-sudo apt install -y nvidia-jetpack
-sudo apt install -y ncdu
+sudo apt install -y git
+#sudo apt install -y nvidia-jetpack
+#sudo apt install ncdu
 
 # Install the pre-built TensorFlow pip wheel
 echo "\e[48;5;202m Install the pre-built TensorFlow pip wheel \e[0m"
@@ -43,7 +44,7 @@ sudo apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip
 sudo apt-get install -y python3-pip
 sudo pip3 install -U pip setuptools
 sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
-sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14
+#sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14
 
 # Install the pre-built PyTorch pip wheel 
 # echo "\e[45m Install the pre-built PyTorch pip wheel  \e[0m"
@@ -51,6 +52,11 @@ sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/
 # wget https://nvidia.box.com/shared/static/phqe92v26cbhqjohwtvxorrwnmrnfx1o.whl -O torch-1.3.0-cp36-# cp36m-linux_aarch64.whl
 # sudo pip3 install numpy torch-1.3.0-cp36-cp36m-linux_aarch64.whl
 
+# Install Jetcam
+git clone https://github.com/NVIDIA-AI-IOT/jetcam
+cd jetcam
+sudo python3 setup.py install
+cd ..
 # Install torchvision package
 # echo "\e[45m Install torchvision package \e[0m"
 # git clone https://github.com/pytorch/vision
@@ -71,7 +77,6 @@ echo "\e[48;5;172m Install traitlets \e[0m"
 # sudo python3 -m pip install git+https://github.com/ipython/traitlets@master
 sudo pip3 install traitlets
 
-
 # Install Jupyter Lab
 echo "\e[48;5;172m Install Jupyter Lab \e[0m"
 sudo apt install -y nodejs npm
@@ -81,8 +86,8 @@ sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter lab --generate-config
 python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
 
-# fix for Traitlet permission error
-sudo chown -R jetson:jetson ~/.local/share/
+# # fix for Traitlet permission error
+# sudo chown -R jetson:jetson ~/.local/share/
 
 # Install jetcard
 #echo "\e[44m Install jetcard \e[0m"
@@ -105,62 +110,57 @@ sudo chown -R jetson:jetson ~/.local/share/
 #sudo systemctl start jetcard_jupyter
 
 # Make swapfile
-echo "\e[46m Make swapfile \e[0m"
-cd
-if [ ! -f /var/swapfile ]; then
-	sudo fallocate -l 6G /var/swapfile
-	sudo chmod 600 /var/swapfile
-	sudo mkswap /var/swapfile
-	sudo swapon /var/swapfile
-	sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
-else
-	echo "Swapfile already exists"
-fi
+# echo "\e[46m Make swapfile \e[0m"
+# cd
+# if [ ! -f /var/swapfile ]; then
+# 	sudo fallocate -l 6G /var/swapfile
+# 	sudo chmod 600 /var/swapfile
+# 	sudo mkswap /var/swapfile
+# 	sudo swapon /var/swapfile
+# 	sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
+# else
+# 	echo "Swapfile already exists"
+# fi
 
-# Install TensorFlow models repository
-echo "\e[48;5;202m Install TensorFlow models repository \e[0m"
-cd
-url="https://github.com/tensorflow/models"
-tf_models_dir="TF-models"
-if [ ! -d "$tf_models_dir" ] ; then
-	git clone $url $tf_models_dir
-	cd "$tf_models_dir"/research
-	git checkout 5f4d34fc
-	wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
-	# wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
-	unzip protobuf.zip
-	./bin/protoc object_detection/protos/*.proto --python_out=.
-	sudo python3 setup.py install
-	cd slim
-	sudo python3 setup.py install
-fi
+# # Install TensorFlow models repository
+# echo "\e[48;5;202m Install TensorFlow models repository \e[0m"
+# cd
+# url="https://github.com/tensorflow/models"
+# tf_models_dir="TF-models"
+# if [ ! -d "$tf_models_dir" ] ; then
+# 	git clone $url $tf_models_dir
+# 	cd "$tf_models_dir"/research
+# 	git checkout 5f4d34fc
+# 	wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
+# 	# wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+# 	unzip protobuf.zip
+# 	./bin/protoc object_detection/protos/*.proto --python_out=.
+# 	sudo python3 setup.py install
+# 	cd slim
+# 	sudo python3 setup.py install
+# fi
 
 # Disable syslog to prevent large log files from collecting
 #sudo service rsyslog stop
 #sudo systemctl disable rsyslog
 
-# Install jupyter_clickable_image_widget
-echo "\e[42m Install jupyter_clickable_image_widget \e[0m"
-cd
-sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
-sudo apt-get install npm
-git clone https://github.com/jaybdub/jupyter_clickable_image_widget
-cd jupyter_clickable_image_widget
-git checkout no_typescript
-sudo pip3 install -e .
-sudo jupyter labextension install js
-sudo jupyter lab build
+# # Install jupyter_clickable_image_widget
+# echo "\e[42m Install jupyter_clickable_image_widget \e[0m"
+# cd
+# sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
+# sudo apt-get install npm
+# git clone https://github.com/jaybdub/jupyter_clickable_image_widget
+# cd jupyter_clickable_image_widget
+# git checkout no_typescript
+# sudo pip3 install -e .
+# sudo jupyter labextension install js
+# sudo jupyter lab build
 
 
-# Install remaining dependencies for projects
-echo "\e[104m Install remaining dependencies for projects \e[0m"
-sudo apt-get install python-setuptools
+# # Install remaining dependencies for projects
+# echo "\e[104m Install remaining dependencies for projects \e[0m"
+# sudo apt-get install python-setuptools
 
-echo "\e[42m Last Package! \e[0m"
-git clone https://github.com/NVIDIA-AI-IOT/jetcam
-cd jetcam
-sudo python3 setup.py install
-cd ..
 
 echo "\e[42m All done! \e[0m"
 
